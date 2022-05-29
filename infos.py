437b533c0ec2bb4b2990c78387d5a78e7ec5f065
haobo724 <s724724@gmail.com>
2022-05-29 22:14:27 +0200

    update

--- infos.py
+++ infos.py
@@ -1,11 +1,13 @@
-import tkinter as tk  # 导入 Tkinter 库
+import tkinter as tk
 import time
 import asyncio
 import threading
 import queue
 import numpy as np
+from contextlib import suppress
 
 q = queue.Queue()
+stop_event = threading.Event()
 
 
 class Form:
@@ -16,7 +18,8 @@ class Form:
 
         self.button = tk.Button(self.root, text="Testbutton1", command=self.change_form_state)
         self.button2 = tk.Button(self.root, text="Testbutton2", command=self.dummy_func2)
-        self.button3 = tk.Button(self.root, text="Close", command=self.root.quit)
+        # self.button3 = tk.Button(self.root, text="Close", command=self.root.quit)
+        self.button3 = tk.Button(self.root, text="Close", command=self.shut_down)
         self.label = tk.Label(master=self.root, text="label1")
         self.label2 = tk.Label(master=self.root, text="label2")
 
@@ -28,11 +31,11 @@ class Form:
         self.new_loop = asyncio.new_event_loop()
         self.new_loop2 = asyncio.new_event_loop()
         self.t = threading.Thread(target=self.get_loop, args=(self.new_loop,), name='thread1')  # 通过当前线程开启新的线程去启动事件循环
-        # self.t2 = threading.Thread(target=self.get_loop, args=(self.new_loop2,), name='thread1')  # 通过当前线程开启新的线程去启动事件循环
+        self.t2 = threading.Thread(target=self.get_loop, args=(self.new_loop2,), name='thread2')  # 通过当前线程开启新的线程去启动事件循环
         self.t.daemon = 1
-        # self.t2.daemon = 1
+        self.t2.daemon = 1
         self.t.start()
-        # self.t2.start()
+        self.t2.start()
         self.root.mainloop()
 
     async def dummy_func1(self, text):
@@ -40,30 +43,19 @@ class Form:
         await asyncio.sleep(1)
         self.label["text"] = text
         q.put(lambda: self.show())
-        print(q.qsize())
 
         print(time.time())
 
-    async def dummy_func3(self, text):
-        await asyncio.sleep(1)
-        self.label2["text"] = text
-        while True:
-            print('what1!')
-            try:
-                fn = q.get()
-            except queue.Empty:
-                await asyncio.sleep(1)
-                fn = None
-                self.label2["text"] = 'Empty'
-
-                break
-            fn()
-        print(time.time())
 
     def dummy_func2(self):
         coroutine = self.dummy_func3('信号')
-        print(time.time)
+        stop_event.clear()
         asyncio.run_coroutine_threadsafe(coroutine,self.new_loop2)  # 这几个是关键，代表在新线程中事件循环不断“游走”执行
+
+    async def dummy_func3(self, text):
+        self.label2["text"] = text
+        while not stop_event.is_set():
+            print('what1!')
 
     def show(self):
         self.label2["text"] = 'show'
@@ -74,8 +66,22 @@ class Form:
 
     def change_form_state(self):
         coroutine1 = self.dummy_func1(200)
-        asyncio.run_coroutine_threadsafe(coroutine1, self.new_loop)  # 这几个是关键，代表在新线程中事件循环不断“游走”执行
+        # asyncio.run_coroutine_threadsafe(coroutine1, self.new_loop)  # 这几个是关键，代表在新线程中事件循环不断“游走”执行
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(coroutine1)
 
-
+    def shut_down(self):
+        # self.t.join()
+        # loop = asyncio.get_event_loop()
+        #
+        stop_event.set()
+        # pending = asyncio.all_tasks()
+        # for task in pending:
+        #     task.cancel()
+        #     # Now we should await task to execute it's cancellation.
+        #     # Cancelled task raises asyncio.CancelledError that we can suppress:
+        #     with suppress(asyncio.CancelledError):
+        #         self.new_loop.run_until_complete(task)
+        self.root.quit()
 if __name__ == '__main__':
     form = Form()
